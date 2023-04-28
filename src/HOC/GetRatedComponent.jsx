c788e039084b8010466970a2af3a6d1fdc06e028
Anton Trifonov <toha62@inbox.ru>
2023-04-28 23:15:39 +0300

    format code

--- src/HOC/GetRatedComponent.jsx
+++ src/HOC/GetRatedComponent.jsx
@@ -1,15 +1,15 @@
 export default function GetRatedComponent(LowRateComponent, HighRateComponent) {
   function Blanc(props) {
-      return <div>{props.children}</div>;
+    return <div>{props.children}</div>;
   }
 
   return function (rating) {        
-      if (rating < 100) {
-          return LowRateComponent;
-      }
-      if (rating > 1000) {
-          return HighRateComponent;
-      }
-      return Blanc;
+    if (rating < 100) {
+      return LowRateComponent;
+    }
+    if (rating > 1000) {
+      return HighRateComponent;
+    }
+    return Blanc;
   }
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -6,19 +6,19 @@ import Video from './Video';
 
 export default function List(props) {
   return props.list.map(item => {
-      const RatedComponent = GetRatedComponent(New, Popular)(+item.views);        
-      const key = Date.now() + Math.random();
+    const RatedComponent = GetRatedComponent(New, Popular)(+item.views);        
+    const key = Date.now() + Math.random();
 
-      switch (item.type) {
-          case 'video':                             
-              return (
-                  <RatedComponent key={key} ><Video {...item} /></RatedComponent>
-              );
+    switch (item.type) {
+      case 'video':                             
+        return (
+          <RatedComponent key={key} ><Video {...item} /></RatedComponent>
+        );
 
-          case 'article':
-              return (
-                  <RatedComponent key={key} ><Article {...item} /></RatedComponent>
-              );
-      }
+      case 'article':
+        return (
+          <RatedComponent key={key} ><Article {...item} /></RatedComponent>
+        );
+    }
   });
 };
